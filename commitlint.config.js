9ca44c78b6fa4a5a3085b2ac0122e2aac1b5d195
Berend de Boer <berend@pobox.com>
2025-12-09 09:41:34 +1300

    vendor: update commitlint and prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "docs",
+        "feat",
+        "fix",
+        "chore",
+        "ci",
+        "vendor",
+        "build",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 }
